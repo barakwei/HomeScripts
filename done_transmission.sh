849e7f505b2dd31dd50374187969e56b10608ce0
Barak <barakwei@gmail.com>
2016-10-23 02:26:18 +0300

    better logs and telegram title in messages

--- done_transmission.sh
+++ done_transmission.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SCRIPT_DIR="$(dirname "$0")"
-"$SCRIPT_DIR/notify.sh" "Download Complete" "$TR_TORRENT_NAME"
+"$SCRIPT_DIR/notify.sh" "Download Complete" "$TR_TORRENT_NAME" >> $SCRIPT_DIR/logs/done_transmission.log 2>&1
--- notify.sh
+++ notify.sh
@@ -3,7 +3,7 @@
 . "$(dirname "$0")"/notify.cfg
 
 if [ "$send_telegram" = "YES" ] ; then
-  curl --data chat_id=$telegram_chat_id --data-urlencode "text=$1: $2" "https://api.telegram.org/bot$telegram_token/sendMessage"
+  curl --data chat_id=$telegram_chat_id --data parse_mode="Markdown" --data-urlencode "text=*$1*: $2" "https://api.telegram.org/bot$telegram_token/sendMessage"
 fi
 
 if [ "$send_pushbullet" = "YES" ] ; then
