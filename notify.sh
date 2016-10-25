1e5597fa8f699c7004a509898f957f992aed9f8a
Barak <barakwei@gmail.com>
2016-10-25 23:47:59 +0300

    Don't show output for notification shell script

--- notify.sh
+++ notify.sh
@@ -3,9 +3,9 @@
 . "$(dirname "$0")"/notify.cfg
 
 if [ "$send_telegram" = "YES" ] ; then
-  curl --data chat_id=$telegram_chat_id --data parse_mode="Markdown" --data-urlencode "text=*$1*: $2" "https://api.telegram.org/bot$telegram_token/sendMessage"
+  curl -s -o /dev/null --data chat_id=$telegram_chat_id --data parse_mode="Markdown" --data-urlencode "text=*$1*: $2" "https://api.telegram.org/bot$telegram_token/sendMessage"
 fi
 
 if [ "$send_pushbullet" = "YES" ] ; then
-  curl https://api.pushbullet.com/v2/pushes -u $pushbullet_token -d type="note" -d title="$1" -d body="$2" -X POST
+  curl -s -o /dev/null https://api.pushbullet.com/v2/pushes -u $pushbullet_token -d type="note" -d title="$1" -d body="$2" -X POST
 fi
